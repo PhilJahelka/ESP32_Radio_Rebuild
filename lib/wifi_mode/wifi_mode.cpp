1bd3e358a13cd8184b743f4ea509138000f083ae
Phillp Jahelka <phillip.jahelka@gmail.com>
2024-01-11 04:02:08 -0800

    todos

--- lib/wifi_mode/wifi_mode.cpp
+++ lib/wifi_mode/wifi_mode.cpp
@@ -248,3 +248,32 @@ void build_station_list(){
     custom_URL = NVS.getString(NVS_STAT).c_str();
     stations.push_back(make_pair("Custom", custom_URL.c_str()));
 }
+
+// optional
+void audio_info(const char *info){
+    Serial.print("info        "); Serial.println(info);
+}
+void audio_id3data(const char *info){  //id3 metadata
+    Serial.print("id3data     ");Serial.println(info);
+}
+void audio_eof_mp3(const char *info){  //end of file
+    Serial.print("eof_mp3     ");Serial.println(info);
+}
+void audio_showstation(const char *info){
+    Serial.print("station     ");Serial.println(info);
+}
+void audio_showstreamtitle(const char *info){
+    Serial.print("streamtitle ");Serial.println(info);
+}
+void audio_bitrate(const char *info){
+    Serial.print("bitrate     ");Serial.println(info);
+}
+void audio_commercial(const char *info){  //duration in sec
+    Serial.print("commercial  ");Serial.println(info);
+}
+void audio_icyurl(const char *info){  //homepage
+    Serial.print("icyurl      ");Serial.println(info);
+}
+void audio_lasthost(const char *info){  //stream URL played
+    Serial.print("lasthost    ");Serial.println(info);
+}
